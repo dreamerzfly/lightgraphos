cdf6ee491983426643cb9ec1e9fe3409debcc871
Mark Iglesias <mark.iglesias@gmail.com>
2014-01-10 18:41:51 -0800

    fixed welcome email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,16 @@ Lightgraphos::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    #domain:               'example.com',
+    user_name:            'graphoslight@gmail.com',
+    password:             'lightgraphos',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'no-replay@example.com'}
 end
