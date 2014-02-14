7fe787e8a331566deed2dd2446da1cf4f4c08826
Mark Iglesias <mark.iglesias@gmail.com>
2014-02-13 19:57:54 -0800

    added table for subscriber

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131001192446) do
+ActiveRecord::Schema.define(version: 20140214034509) do
 
   create_table "gears", force: true do |t|
     t.integer  "user_id"
@@ -32,6 +32,12 @@ ActiveRecord::Schema.define(version: 20131001192446) do
     t.string   "component"
     t.decimal  "min_price"
     t.decimal  "max_price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "subscribers", force: true do |t|
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
